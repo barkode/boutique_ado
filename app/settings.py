ca7328fe6043ef317cf43f098beb1b93671b0678
barkode <konstantinks@gmail.com>
2024-09-30 16:38:36 +0100

    Load settings from environment variables
    Added environment variable support using python-dotenv. This change secures sensitive data like the secret key and allows control of other settings through a .env file. Debug mode and site ID are also now configurable via environment variables.

--- app/settings.py
+++ app/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Take environment variables from .env.
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wns0(&6s!9qbtla9r*$y-u$daz531!$r+t%7nr3ck-y+gtwr4$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = []
 
@@ -88,8 +92,7 @@ AUTHENTICATION_BACKENDS = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-SITE_ID = 1
-
+SITE_ID = os.getenv('SITE_ID', 1)
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
