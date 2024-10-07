ab841fd45f620f89e0e90455aa4a0158e191610f
barkode <konstantinks@gmail.com>
2024-10-07 11:16:50 +0100

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q7Cly2NvNdv2IMkgkY5lUOWlIE4bkiUs4QG2wEBiXuSqeMXL2tmBhHFkndWQ1NV0g9JwMY9fa6KHQfzku5PBZF000vTJtuSBZ',
+        'client_secret': 'test client secret',
         }
 
     return render(request, template, context)
